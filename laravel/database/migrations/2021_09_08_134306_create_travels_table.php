2d255355998a959772b1a57117ebb4d4c2e5a107
Gianluca <gl.galassini@gmail.com>
2021-09-08 16:08:58 +0200

    tabella viaggi

--- laravel/database/migrations/2021_09_08_134306_create_travels_table.php
+++ laravel/database/migrations/2021_09_08_134306_create_travels_table.php
@@ -15,6 +15,16 @@ class CreateTravelsTable extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('departs_from', 30);
+            $table->tinyInteger('flight_included')->unsigned();
+            $table->tinyInteger('duration')->unsigned();
+            $table->tinyInteger('max_participants')->unsigned();
+            $table->tinyInteger('available_places')->unsigned();
+            $table->text('description');
+            $table->string('tour_operator', 40);
+            $table->string('tour_leader', 30);
+            $table->float('price_person', 8,2);
             $table->timestamps();
         });
     }
