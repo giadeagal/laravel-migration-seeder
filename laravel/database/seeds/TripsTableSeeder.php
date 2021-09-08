cbfe127af712cef907d9e8fc67aba6ae4c636020
Gianluca <gl.galassini@gmail.com>
2021-09-08 19:18:27 +0200

    fine

--- laravel/database/seeds/TripsTableSeeder.php
+++ laravel/database/seeds/TripsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Trip;
 
 class TripsTableSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class TripsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $newTrip = new Trip();
+            $newTrip->name = "viaggio bello";
+            $newTrip->departs_from = "Roma";
+            $newTrip->flight_included = rand(0,1);
+            $newTrip->duration = rand(2, 30);
+            $newTrip->max_participants = rand(10, 30);
+            $newTrip->available_places = rand(1, 10);
+            $newTrip->description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quis nam voluptatem laboriosam maiores";
+            $newTrip->tour_operator = "Boscolo";
+            $newTrip->tour_leader = "Gianluca";
+            $newTrip->price_person = rand(45, 50000);
+
+            $newTrip->save();
+        }
     }
 }
